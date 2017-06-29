68f2d6cf45e003e033c11b42c78dfa68ce459bff
phi16 <phi16.256@gmail.com>
2017-06-29 11:22:08 +0900

    Initial Commit

---
+++ src-noconflict/mode-ex3.js
@@ -0,0 +1,82 @@
+ace.define("ace/mode/ex3_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"], function(require, exports, module) {
+"use strict";
+
+var oop = require("../lib/oop");
+var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
+
+var Ex3HighlightRules = function() {
+    var defIns = "AND|ADD|LDA|STA|BUN|BSA|ISZ|CLA|CLE|CMA|CME|CIR|CIL|INC|SPA|SNA|SZA|SZE|HLT";
+    var valIns = "HEX|DEC|CHR|SYM|ORG|END|I"
+    var depIns = "INP|OUT|SKI|SKO|ION|IOF|SIO|PIO|IMK";
+    var newIns = "SEG|SPR|WRT|TRS|ROT";
+
+    var keywordMapper = this.createKeywordMapper({
+        "support.function": newIns,
+        "comment" : depIns,
+        "keyword": defIns,
+        "constant.language": valIns,
+    }, "identifier", true);
+
+    this.$rules = {
+        "start" : [ {
+            token : "comment",
+            regex : "/.*$"
+        },{
+            token : "string",           // " string
+            regex : '".*?"'
+        }, {
+            token : "string",           // ' string
+            regex : "'.*?'"
+        }, {
+            token : "constant.language",
+            regex : "HEX\\b",
+            next : [
+                { token : "constant.numeric", regex : "[+-]?(?:\\d|[A-F])+(?:(?:\\.(?:\\d|[A-F])*)?(?:[eE][+-]?\\d+)?)?\\b", next: "start" },
+                { defaultToken : "identifier" }
+            ]
+        }, {
+            token : keywordMapper,
+            regex : "(?:I|[A-Z]{3})\\b"
+        }, {
+            token : "identifier",
+            regex : "[a-zA-Z_$][a-zA-Z0-9_$]*\\b"
+        }, {
+            token : "constant.numeric",
+            regex : "[+-]?(?:\\d|[A-F])+(?:(?:\\.(?:\\d|[A-F])*)?(?:[eE][+-]?\\d+)?)?\\b"
+        }, {
+            token : "text",
+            regex : "\\s+"
+        } ]
+    };
+    this.normalizeRules();
+};
+
+oop.inherits(Ex3HighlightRules, TextHighlightRules);
+
+exports.Ex3HighlightRules = Ex3HighlightRules;
+});
+
+ace.define("ace/mode/ex3",["require","exports","module","ace/lib/oop","ace/mode/text","ace/mode/ex3_highlight_rules","ace/range"], function(require, exports, module) {
+"use strict";
+
+var oop = require("../lib/oop");
+var TextMode = require("./text").Mode;
+var Ex3HighlightRules = require("./ex3_highlight_rules").Ex3HighlightRules;
+var Range = require("../range").Range;
+
+var Mode = function() {
+    this.HighlightRules = Ex3HighlightRules;
+    this.$behaviour = this.$defaultBehaviour;
+};
+oop.inherits(Mode, TextMode);
+
+(function() {
+
+    this.lineCommentStart = "/";
+
+    this.$id = "ace/mode/ex3";
+}).call(Mode.prototype);
+
+exports.Mode = Mode;
+
+});
